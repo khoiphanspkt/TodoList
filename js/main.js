738340a34fab21d469377aacc9f86b6e85e43139
khoiphanspkt <khoiphanspkt@gmail.com>
2018-07-25 16:53:01 +0700

    finish project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,124 @@
-//user click on the 'add' button
-// if there is any text inside the item field, add that text to the todo list
-document.getElementById("addItem").addEventListener('click', function() {
-    var value = document.getElementById("item").value;
-    console.log(value);
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
+    todo: [],
+    completed: []
+};
+
+// remove and complete icon in SVG format
+var removeSVG = '<svg enable-background="new 0 0 40 40" version="1.1" viewBox="0 0 40 40" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path class="fill" d="M28,40H11.8c-3.3,0-5.9-2.7-5.9-5.9V16c0-0.6,0.4-1,1-1s1,0.4,1,1v18.1c0,2.2,1.8,3.9,3.9,3.9H28c2.2,0,3.9-1.8,3.9-3.9V16 c0-0.6,0.4-1,1-1s1,0.4,1,1v18.1C33.9,37.3,31.2,40,28,40z"/></g><g><path class="fill" d="M33.3,4.9h-7.6C25.2,2.1,22.8,0,19.9,0s-5.3,2.1-5.8,4.9H6.5c-2.3,0-4.1,1.8-4.1,4.1S4.2,13,6.5,13h26.9 c2.3,0,4.1-1.8,4.1-4.1S35.6,4.9,33.3,4.9z M19.9,2c1.8,0,3.3,1.2,3.7,2.9h-7.5C16.6,3.2,18.1,2,19.9,2z M33.3,11H6.5 c-1.1,0-2.1-0.9-2.1-2.1c0-1.1,0.9-2.1,2.1-2.1h26.9c1.1,0,2.1,0.9,2.1,2.1C35.4,10.1,34.5,11,33.3,11z"/></g><g><path class="fill" d="M12.9,35.1c-0.6,0-1-0.4-1-1V17.4c0-0.6,0.4-1,1-1s1,0.4,1,1v16.7C13.9,34.6,13.4,35.1,12.9,35.1z"/></g><g><path class="fill" d="M26.9,35.1c-0.6,0-1-0.4-1-1V17.4c0-0.6,0.4-1,1-1s1,0.4,1,1v16.7C27.9,34.6,27.4,35.1,26.9,35.1z"/></g><g><path class="fill" d="M19.9,35.1c-0.6,0-1-0.4-1-1V17.4c0-0.6,0.4-1,1-1s1,0.4,1,1v16.7C20.9,34.6,20.4,35.1,19.9,35.1z"/></g></svg>';
+var completeSVG = '<svg baseProfile="tiny" height="32px" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Guides__x26__Forms"/><g id="Icons"><g><polygon points="22.186,10.52 14.054,18.652 9.814,14.411 8.4,15.825 14.054,21.48 23.6,11.934   "/></g></g></svg>';
+
+
+renderTodoList();
+
+// User clicked on the add button
+// If there is any text inside the item field, add that text to the todo list
+document.getElementById('addItem').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    if (!value) {
+        alert("Do not insert blank context !!!");
+    } else if (value) {
+        addItem(value);
+    }
 });
+
+document.getElementById('item').addEventListener('keydown', function(e) {
+    var value = this.value;
+    if ((e.code === 'Enter' || e.code === 'NumpadEnter') && value) {
+        addItem(value);
+    }
+});
+
+function addItem(value) {
+    addItemToDOM(value);
+    document.getElementById('item').value = '';
+
+    data.todo.push(value);
+    dataObjectUpdated();
+}
+
+function renderTodoList() {
+    if (!data.todo.length && !data.completed.length) return;
+
+    for (var i = 0; i < data.todo.length; i++) {
+        var value = data.todo[i];
+        addItemToDOM(value);
+    }
+
+    for (var j = 0; j < data.completed.length; j++) {
+        var value = data.completed[j];
+        addItemToDOM(value, true);
+    }
+}
+
+function dataObjectUpdated() {
+    localStorage.setItem('todoList', JSON.stringify(data));
+}
+
+function removeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
+
+    if (id === 'todo') {
+        data.todo.splice(data.todo.indexOf(value), 1);
+    } else {
+        data.completed.splice(data.completed.indexOf(value), 1);
+    }
+    dataObjectUpdated();
+
+    parent.removeChild(item);
+}
+
+function completeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
+
+    if (id === 'todo') {
+        data.todo.splice(data.todo.indexOf(value), 1);
+        data.completed.push(value);
+    } else {
+        data.completed.splice(data.completed.indexOf(value), 1);
+        data.todo.push(value);
+    }
+    dataObjectUpdated();
+
+    // Check if the item should be added to the completed list or to re-added to the todo list
+    var target = (id === 'todo') ? document.getElementById('completed') : document.getElementById('todo');
+
+    parent.removeChild(item);
+    target.insertBefore(item, target.childNodes[0]);
+}
+
+// Adds a new item to the todo list
+function addItemToDOM(text, completed) {
+    var list = (completed) ? document.getElementById('completed') : document.getElementById('todo');
+
+    var item = document.createElement('li');
+    item.innerText = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeSVG;
+
+    // Add click event for removing the item
+    remove.addEventListener('click', removeItem);
+
+    var complete = document.createElement('button');
+    complete.classList.add('complete');
+    complete.innerHTML = completeSVG;
+
+    // Add click event for completing the item
+    complete.addEventListener('click', completeItem);
+
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    item.appendChild(buttons);
+
+    list.insertBefore(item, list.childNodes[0]);
+}
